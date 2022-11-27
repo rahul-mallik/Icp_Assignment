ebc4c0505c161bc9db255014e0bd9b51d41aa737
Rahul Mallik <117148280+rahul-mallik@users.noreply.github.com>
2022-11-27 23:37:20 +0530

    Update Q6.java

--- icp_lab1/Q6.java
+++ icp_lab1/Q6.java
@@ -1,5 +1,9 @@
 package icp_lab1;
 
+//  Write a java program to store your Bank account no, name and balance in three different
+//  variables and display their value on the screen as given:
+//  “My name is Amit Kumar bearing account number 123456 having balance 7654.98”
+
 public class Q6 {
     public static void main(String[] args) {
 //        “My name is Amit Kumar bearing account number 123456
