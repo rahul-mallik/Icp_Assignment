cee1754e05f50fa2986f263439dc3119416b44a0
Rahul Mallik <117148280+rahul-mallik@users.noreply.github.com>
2022-11-27 23:34:15 +0530

    Add files via upload

---
+++ icp_lab1/Q6.java
@@ -0,0 +1,12 @@
+package icp_lab1;
+
+public class Q6 {
+    public static void main(String[] args) {
+//        “My name is Amit Kumar bearing account number 123456
+//        having balance 7654.98”
+        String name = "Rahul Mallik"; //your name
+        int acc = 123456; // account no.
+        float balance = 7654.98f; // account balance
+        System.out.println("My name is "+name+" bearing account number "+acc+" having balance "+balance);
+    }
+}
