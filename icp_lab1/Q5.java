4f2d8b84e095a973eb0042d29f8d7889bf64f748
Rahul Mallik <117148280+rahul-mallik@users.noreply.github.com>
2022-11-27 23:12:27 +0530

    Update Q5.java

--- icp_lab1/Q5.java
+++ icp_lab1/Q5.java
@@ -1,12 +1,9 @@
 package icp_lab1;
 
-import java.util.*;
 public class Q5 {
     public static void main(String[] args){
-        Scanner scn =  new Scanner(System.in);
-        String name = scn.nextLine();
-        System.out.print("Hey "+name+", Good Morning !!");
-
-        scn.close();
+        String a = "Hey Rahul Mallik"
+        String b = ", Good Morning !!"
+        System.out.print(a+b);
     }
 }
