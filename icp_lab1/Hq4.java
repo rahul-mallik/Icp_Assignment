3bf0fb9950821808cf9a425bf8bed047ffa488d6
Rahul Mallik <117148280+rahul-mallik@users.noreply.github.com>
2022-12-05 21:02:40 +0530

    Create Hq4.java

---
+++ icp_lab1/Hq4.java
@@ -0,0 +1,28 @@
+public class Hq4 {
+    public static void main(String[] args) {
+/*Suppose that a variable a is declared as double a = 3.14159. What do each of the following 
+print?
+a)System.out.println(a);
+b)System.out.println(a+1);
+c)System.out.println(8/(int) a);
+d)System.out.println(8/a);
+e)System.out.println((int) (8/a));
+*/
+
+        double a = 3.14159;
+        System.out.println(a);
+        System.out.println(a+1);
+        System.out.println(8/(int) a);
+        System.out.println(8/a);
+        System.out.println((int) (8/a));
+
+    /*
+         Output
+        3.14159
+        4.14159
+        2
+        2.5464812403910124
+        2
+         */
+    }
+}
