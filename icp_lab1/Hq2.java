f66f42c1a5a700a38dd2ceeddfca2279c48c04f4
Rahul Mallik <117148280+rahul-mallik@users.noreply.github.com>
2022-12-05 21:01:47 +0530

    Create Hq2.java

---
+++ icp_lab1/Hq2.java
@@ -0,0 +1,19 @@
+public class Hq2 {
+    public static void main(String[] args) {
+        /*What do each of the following print?
+        a)System.out.println('b');
+        b)System.out.println('b' + 'c');
+        c)System.out.println((char) ('a' + 4)); */
+
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char) ('a' + 4));
+
+        /*
+        Output
+        b
+        197
+        e
+         */
+    }
+}
