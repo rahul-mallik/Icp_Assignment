aaee3149f5bfa4912233a6e862e984145807a73f
Rahul Mallik <117148280+rahul-mallik@users.noreply.github.com>
2023-01-07 21:50:13 +0530

    Update Q10.java

--- icp_assignment_03/Q10.java
+++ icp_assignment_03/Q10.java
@@ -1,4 +1,21 @@
-
+import java.util.Scanner;
 public class Q10 {
+  public static void main(String[] args){
+  Scanner in = new Scanner(System.in);
+  int mrks = in.nextInt();
+  int tens = mrks/10;
+  // for convinence using the below codes but for exams follow the old switch case syntax 
+  switch(tens){
+    case 10 -> System.out.print("O");
+    case 9 -> System.out.print("A");
+    case 8 -> System.out.print("B");
+    case 7 -> System.out.print("C");
+    case 6 -> System.out.print("D");
+    case 5 -> System.out.print("E");
+    case 4,3,2,1,0 -> System.out.print("F");
+    default -> System.out.print("Invalid Input :( ");
+  
+  }
+ }
 
 }
