ad3772952226967c49d084097b902aad25459392
Rahul Mallik <117148280+rahul-mallik@users.noreply.github.com>
2023-01-08 11:38:36 +0530

    Add files via upload

---
+++ icp_assignment_03/Hq1.java
@@ -0,0 +1,52 @@
+package icp_lab1;
+
+import java.util.Scanner;
+
+public class Hq1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter (0)Scissor, (1)rock, (2)paper : ");
+        int user = in.nextInt();
+
+        int random = (int)(3*Math.random());
+        System.out.println(random);
+        System.out.println(user);
+        switch (random){
+            case 0 :
+                System.out.print("The Computer choose Scissor.");
+                if (random == user)
+                    System.out.println("You choose Scissor too. Its a draw");
+                else if (user == 1)
+                    System.out.println("You choose rock. You won");
+                else
+                    System.out.println("You choose paper. Computer won");
+                break;
+
+            case 1 :
+                System.out.print("The Computer choose Rock.");
+                if (random == user)
+                    System.out.println("You choose Rock too. Its a draw");
+                else if (user == 2)
+                    System.out.println("You choose paper. You won");
+                else
+                    System.out.println("You choose scissor. Computer won");
+                break;
+
+            case 2 :
+                System.out.print("The Computer choose paper.");
+                if (random == user)
+                    System.out.println("You choose paper too. Its a draw");
+                else if (user == 1)
+                    System.out.println("You choose rock. You won");
+                else
+                    System.out.println("You choose scissor. Computer won");
+                break;
+
+            default:
+                System.out.println("invalid Input :( ");
+        }
+
+
+
+    }
+}
