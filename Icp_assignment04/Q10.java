55e89e1486fb5fab5bd412c07a163c851ab40e63
Rahul Mallik <117148280+rahul-mallik@users.noreply.github.com>
2023-02-04 12:55:35 +0530

    Update Q10.java

--- Icp_assignment04/Q10.java
+++ Icp_assignment04/Q10.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Q10 {
     public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
-//
-//        System.out.print("Enter a number: ");
-//        int num = in.nextInt(),i = 1 ,k=3;
-//
-//        while(k<num){
-//            k *= 3;
-//            i++;
-//        }
-//
-//        System.out.println("The largest power of 3 less than or equal to 100 is "+(int)(Math.pow(3,i-1)));
+        Scanner in = new Scanner(System.in);
 
-        System.out.println(((25-2)*((int)Math.pow(7,-1)))%26);
+        System.out.print("Enter a number: ");
+        int num = in.nextInt(),i = 1 ,k=3;
+
+        while(k<num){
+            k *= 3;
+            i++;
+        }
+
+        System.out.println("The largest power of 3 less than or equal to 100 is "+(int)(Math.pow(3,i-1)));
+
+
     }
 }
